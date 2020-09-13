e877841ab024cb86803da1c2e19413c5571ae2b9
pedro <pedro-manuel16@hotmail.com>
2020-09-13 14:42:15 +0100

    first commit

---
+++ y/y/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiMendeleyService } from 'src/services/api-mendeley.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'y';
+  
+  constructor(public apiMendeley: ApiMendeleyService) {
+  }
+  ngOnInit(): void {
+
+  }
+
+  authorization() {
+    let state = this.generateRandomString();
+    let client_id = "8688";
+    let redirect_uri = "http://localhost:5000/callback";
+    let client_secret = "X21jDaXfGnmn4Ury";
+    let response_type = "code"
+    let scope = "all";
+    let url = "client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&response_type=" + response_type + "&scope=" + scope + "&state=" + state;
+    this.apiMendeley.mendeleyoAuth(url).subscribe(result => {
+      console.log(result);
+    }, (error => {
+      console.log(error);
+    }))
+  }
+
+  generateRandomString(): string {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+}
